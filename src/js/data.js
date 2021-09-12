aa328fb8aa5c8d6c03e15f42b44ccd9b3b60be55
ac-gomes <acgoms88@gmail.com>
2021-09-11 21:30:25 -0300

    init

--- src/js/data.js
+++ src/js/data.js
@@ -1,1 +1,6 @@
-console.log('teste')
+// GET /forecastrss?location=sunnyvale,ca HTTP/1.1
+// Host: weather-ydn-yql.media.yahoo.com
+// X-Yahoo-App-Id: YOUR_APP_ID
+// Authorization: OAuth
+// oauth_consumer_key="YOUR_CONSUMER_KEY",oauth_signature_method="HMAC-SHA1",oauth_timestamp="YOUR_TIMESTAMP",oauth_nonce="YOUR_NONCE",oauth_version="1.0",oauth_signature="YOUR_GENERATED_SIGNATURE"
+// cache-control: no-cache
